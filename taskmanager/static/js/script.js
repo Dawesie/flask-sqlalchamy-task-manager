92e13b8a1bcc70e72cad4a124b2d4d0998a98193
Dawesie <marie.dawe01967@gmail.com>
2024-01-07 18:07:40 +0000

    Add code to create task using dropdown bar.

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+
+
+    //select initalization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
 });
