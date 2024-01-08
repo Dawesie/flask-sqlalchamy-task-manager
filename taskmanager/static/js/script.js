8c4dc7ee83b58341ffdacaf85baeb588b86e7af6
Dawesie <marie.dawe01967@gmail.com>
2024-01-08 16:50:14 +0000

    Add code to view tasks on the front-end in date order.

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -17,4 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
 
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
 });
