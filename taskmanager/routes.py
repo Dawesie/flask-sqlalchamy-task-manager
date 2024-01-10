203124815006fe87634df68a556beb156eebc211
Dawesie <marie.dawe01967@gmail.com>
2024-01-10 18:02:56 +0000

    Add code to delete tasks.

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -76,3 +76,9 @@ def edit_task(task_id):
 
 
 
+@app.route("/delete_tasky/<int:task_id>")
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("home"))
